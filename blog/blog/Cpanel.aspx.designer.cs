b2cab17519ca739a48c00c6e6cac60479b133818
yuvaloni <yuvon2@gmail.com>
2014-08-12 13:38:48 +0300

    ghmgm

--- blog/blog/Cpanel.aspx.designer.cs
+++ blog/blog/Cpanel.aspx.designer.cs
@@ -40,13 +40,13 @@ namespace blog {
         protected global::System.Web.UI.WebControls.ImageButton logout;
         
         /// <summary>
-        /// Label1 control.
+        /// create control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Label create;
         
         /// <summary>
         /// b control.
